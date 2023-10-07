771cf0b3875781d2d13c934977de98580fbb8935
Anav Gupta <anavguptadelhi@gmail.com>
2023-10-07 22:20:23 +0530

    tried to edit the layout of the dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,68 +1,58 @@
-"use client";
+"use client"
 import React, { useState } from "react";
 import { UserButton, useUser } from "@clerk/nextjs";
-import Image from "next/image";
-import { Banknote, Pill, School } from "lucide-react";
+import { dark } from "@clerk/themes";
+import { ArrowRight } from "lucide-react";
 
 export default function Dashboard() {
   const [isBusiness, setIsBusiness] = useState(false);
   const { isLoaded, isSignedIn, user } = useUser();
 
   return (
-    <div className="flex">
-      <div className="w-1/3 h-screen ml-3 mb-3 bg-black rounded-lg border border-white">
-        <div className="flex items-center justify-center mt-6">
-          <UserButton/>
-        </div>
-        <div className="text-xl text-white font-bold flex items-center justify-center mt-3">
-          Hello {user?.firstName}! Welcome to your dashboard!
-        </div>
-        <div className="flex items-center px-2 mt-8 text-xl font-bold">
-          Matches
+    <div>
+      <div
+        className="w-1/3 ml-3 mb-4 bg-black rounded-lg bg-gradient-to-r from-violet-600 to-violet-500"
+        style={{ height: "35vh" }}
+      >
+        <div className="flex items-center justify-center">
+          <div className="mt-3">
+            <UserButton
+              appearance={{
+                baseTheme: dark,
+              }}
+            />
+          </div>
+          <div className="text-2xl text-white font-extrabold mt-3 py-4 ml-4">
+            Hello {user?.firstName}
+          </div>
         </div>
         <div>
-          <div className="flex items-center px-4 mt-6">
-            <Banknote/>
-            <div className="font-bold text- text-white ml-2">Growmore</div>
+          <div className="mt-3 flex items-center justify-start">
+            <img
+              src="/match.png"
+              className="h-[175px] w-[175px]"
+              alt="Match"
+            />
+            <div>
+              <span className="font-semibold text-xl text-white px-4 flex items-center">
+                Look for Companies Around You
+              </span>
+              <div className="mt-3 flex px-4">
+                <button className="bg-black border border-gray-600 rounded-lg w-[100px] h-[40px]">
+                  <div className="flex items-center justify-between">
+                    <ArrowRight className="h-[20px] w-[20] ml-2" />
+                    <span className="font-light text-sm text-white mr-4">
+                      Matches
+                    </span>
+                  </div>
+                </button>
+              </div>
+            </div>
           </div>
-            <div className="font-semilight text-xs px-4 text-gray-400">Fintech Based Company</div>
-        </div>
-        <div>
-          <div className="flex items-center px-4 mt-4">
-            <Pill/>
-            <div className="font-bold text- text-white ml-2">Healthify</div>
-          </div>
-            <div className="font-semilight text-xs px-4 text-gray-400">Healthcare Based Company</div>
-        </div>
-        <div>
-          <div className="flex items-center px-4 mt-4">
-            <School/>
-            <div className="font-bold text- text-white ml-2">Educare</div>
-          </div>
-            <div className="font-semilight text-xs px-4 text-gray-400">Educational Services Company</div>
         </div>
       </div>
-      <div className="flex flex-col w-2/3 ml-3 mr-3">
-        <div
-          className="flex items-center justify-center w-full bg-black rounded-lg border border-white"
-          style={{ height: "50vh" }}
-        >
-          Hello World Mark II
-        </div>
-        <div className="flex justify-between">
-          <div 
-            className="flex items-center justify-center w-1/2 bg-black mb-3 rounded-lg border border-white mt-3 mb-3"
-            style={{ height: "48.5vh"}}
-          >
-            Hello World Mark III
-          </div>
-          <div 
-            className="flex items-center justify-center w-1/2 bg-black mb-3 rounded-lg border border-white mt-3 mb-3"
-            style={{ height: "48.5vh"}}
-          >
-            Hello World Mark IV
-          </div>
-        </div>
+      <div className="w-1/3 ml-3 mb-3 bg-black rounded-lg bg-gradient-to-r from-violet-600 to-violet-500 mt-4" style={{ height: "65vh" }}>
+
       </div>
     </div>
   );
