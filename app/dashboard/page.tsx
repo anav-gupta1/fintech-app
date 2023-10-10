09360978c0a671384b37e31cd36b02573dc62085
Anav Gupta <anavguptadelhi@gmail.com>
2023-10-10 17:41:21 +0530

    finished the UI (majorly)

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,189 +1,123 @@
 "use client";
-import React, { useState } from "react";
-import { ArrowRight, Info, Search } from "lucide-react";
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  ArcElement,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Doughnut } from "react-chartjs-2";
-import { Line } from "react-chartjs-2";
+import React from "react";
+import { ArrowRight, HelpCircle, Info, Search } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
 
-ChartJS.register(
-  ArcElement,
-  Tooltip,
-  Legend,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement
-);
-
 export default function Dashboard() {
-  const [isBusiness, setIsBusiness] = useState(false);
-  const data = {
-    labels: ["Orange", "Yellow", "Pink"],
-    datasets: [
-      {
-        data: [33, 33, 33], // 33% for each value
-        backgroundColor: ["orange", "yellow", "pink"],
-      },
-    ],
-  };
-
-  const dataline = {
-    labels: ["Investment 1", "Investment 2", "Investment 3"],
-    datasets: [
-      {
-        label: "Investment",
-        data: [0, 10, 15, 17, 18, 21, 24],
-        backgroundColor: ["transparent"],
-        borderColor: ["orange"],
-        pointBorderColor: ["transparent"],
-        pointBorderWidth: 4,
-      },
-    ],
-  };
-
   return (
-    <div
-      className="bg-zinc-800 w-7/8 ml-6 mr-6 w-6/7 rounded-lg"
-      style={{ height: "85vh" }}
-    >
-      <div className="flex items-center justify-start px-4">
-        <h1 className="font-semibold text-2xl mt-4">Dashboard</h1>
+    <div>
+      <div className="flex items-center justify-center mt-4">
+        <h1 className="font-extrabold text-3xl p-4">Dashboard</h1>
       </div>
-      <div className="flex items-center justify-start px-4 mt-4">
-        <div
-          className="w-[450px] rounded-lg ml-8 relative"
-          style={{ height: "200px" }}
-        >
-          <div
-            className="absolute top-0 left-0 w-full h-full rounded-lg"
-            style={{
-              background: "linear-gradient(to top right, transparent, #A0E5C8)",
-            }}
-          ></div>
-          <div className="w-full h-full bg-zinc-900 rounded-lg p-4">
-            <div className="flex items-center justify-center h-[150px]">
-              <UserButton/>
-              <div className="relative ml-2">
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="bg-black border border-white text-white rounded-full pl-10 pr-3 py-2"
-                />
-                <Search
-                  className="absolute left-3 top-1/2 transform -translate-y-1/2 text-white"
-                  size={18}
-                />
-              </div>
+      <div className="flex items-center justify-center">
+        <div className="w-[300px] h-[100px] border border-gray-500 rounded-lg mt-4 ml-4">
+          <h1 className="text-sm font-light px-4 mt-2">User Profile</h1>
+          <div className="flex items-center justify-between">
+            <h1 className="text-lg font-bold px-4 mt-2">Hello Anav!</h1>
+            <div className="mr-4">
+              <UserButton />
             </div>
           </div>
+          <h1 className="text-sm font-extralight px-4 text-gray-400">
+            Manage your profile
+          </h1>
         </div>
-        <div
-          className="w-[450px] rounded-lg ml-8 relative"
-          style={{ height: "200px" }}
-        >
-          <div
-            className="absolute top-0 left-0 w-full h-full rounded-lg"
-            style={{
-              background: "linear-gradient(to top right, transparent, #D08D7B)",
-            }}
-          ></div>
-          <div className="w-full h-full bg-zinc-900 rounded-lg p-4">
-            <div className="flex items-center justify-center h-[150px]">
-              <Info />
-              <span className="text-lg font-light ml-2">
-                Below is a list of companies which you could go through,
-                generated completely by our matching algorithm
-              </span>
+        <div className="w-[300px] h-[100px] border border-gray-500 rounded-lg mt-4 ml-4">
+          <h1 className="text-sm font-light px-4 mt-2">News Search</h1>
+          <div className="flex items-center justify-between">
+            <h1 className="text-lg font-bold px-4 mt-2">Look for articles!</h1>
+            <div className="mr-4">
+              <Search />
             </div>
           </div>
+          <button className="ml-4 text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-xs px-2 py-1 text-center">
+            Look!
+          </button>
         </div>
-        <div
-          className="w-[450px] rounded-lg ml-8 relative"
-          style={{ height: "200px" }}
-        >
-          <div
-            className="absolute top-0 left-0 w-full h-full rounded-lg"
-            style={{
-              background: "linear-gradient(to top right, transparent, #54349C)",
-            }}
-          ></div>
-          <div className="w-full h-full bg-zinc-900 rounded-lg p-4">
-            <div className="flex items-center justify-center h-[100px]">
-              <Info />
-              <span className="text-lg font-light ml-2">
-                Click here to get some information about the latest stocks
-              </span>
+        <div className="w-[300px] h-[100px] border border-gray-500 rounded-lg mt-4 ml-4">
+          <h1 className="text-sm font-light px-4 mt-2">Need help?</h1>
+          <div className="flex items-center justify-between">
+            <h1 className="text-lg font-bold px-4 mt-2">Contact Us!</h1>
+            <div className="mr-4">
+              <HelpCircle />
             </div>
-            <div className="flex items-center justify-center">
-              <button className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
-                News
-              </button>
-            </div>
+          </div>
+          <h1 className="text-sm font-extralight px-4 text-gray-400">
+            venturely@gmail.com
+          </h1>
+        </div>
+      </div>
+      <div className="flex items-center justify-center mt-4">
+        <div className="relative w-[400px]">
+          <input
+            type="text"
+            className="w-full bg-transparent border border-gray-400 rounded-lg pl-4 pr-10 py-2"
+            style={{ height: "40px" }}
+            placeholder="Search for companies"
+          />
+          <div className="absolute top-0 right-0 flex items-center h-full pr-3">
+            <Search />
           </div>
         </div>
       </div>
-
-      <div className="flex items-center justify-center mt-4 overflow-x-auto">
-        <table className="border border-white">
-          <thead className="bg-zinc-800">
+      <div className="overflow-x-auto mt-4 ml-4 mr-4 rounded-lg">
+        <table className="min-w-full">
+          <thead className="bg-gray-600">
             <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Gender</th>
-              <th>City</th>
-              <th>Country</th>
-              <th>Age</th>
-              <th>Years of Experience</th>
-              <th>College Degree</th>
-              <th>University Name</th>
-              <th>University Tier</th>
-              <th>Company Name</th>
-              <th>Industry</th>
-              <th>Sector</th>
-              <th>Company Bio</th>
-              <th>Location</th>
-              <th>Valuation</th>
-              <th>Profitable</th>
-              <th>Growth Rate</th>
-              <th>Funding Round</th>
-              <th>Investor Number</th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-100 uppercase tracking-wider border-r border-gray-400">
+                Company Name
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-100 uppercase tracking-wider border-r border-gray-400">
+                Company Email
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-100 uppercase tracking-wider border-r border-gray-400">
+                Country
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-100 uppercase tracking-wider border-r border-gray-400">
+                Description
+              </th>
+              <th className="px-6 py-3 text-left text-xs font-medium text-gray-100 uppercase tracking-wider">
+                Industry
+              </th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>Shreedhar</td>
-              <td>Raj</td>
-              <td>i'mshreedharraj@ilikeshiv.com</td>
-              <td>Trans</td>
-              <td>New Delhi</td>
-              <td>India</td>
-              <td>25</td>
-              <td>5</td>
-              <td>Yes</td>
-              <td>Mahipalpur Institute of Technology</td>
-              <td>10</td>
-              <td>LetsLeap</td>
-              <td>Technology</td>
-              <td>Software</td>
-              <td>Leap towards a better future</td>
-              <td>Mahipalpuri Garage</td>
-              <td>$100M</td>
-              <td>Yes</td>
-              <td>10%</td>
-              <td>Series A</td>
-              <td>5</td>
+              <td className="px-6 py-4 whitespace-wrap border-r border-b border-gray-400 font-semibold">
+                Shreedhar Industries
+              </td>
+              <td className="px-6 py-4 whitespace-wrap border-r border-b border-gray-400 text-gray-400">
+                itsshreedharraj@gmail.com
+              </td>
+              <td className="px-6 py-4 whitespace-wrap border-r border-b border-gray-400 text-gray-400">
+                India
+              </td>
+              <td className="px-6 py-4 whitespace-wrap border-r border-b border-gray-400 text-gray-400">
+                Prithvi-Shreedharraj Chauhan
+              </td>
+              <td className="px-6 py-4 whitespace-wrap border-b border-gray-400 text-gray-400">
+                Transgender trafficking
+              </td>
             </tr>
+            <tr>
+              <td className="px-6 py-4 whitespace-wrap border-r border-b border-gray-400 font-semibold">
+                Aaryan Chadha
+              </td>
+              <td className="px-6 py-4 whitespace-wrap border-r border-b border-gray-400 text-gray-400">
+                aaryanchadha@gmail.com
+              </td>
+              <td className="px-6 py-4 whitespace-wrap border-r border-b border-gray-400 text-gray-400">
+                India
+              </td>
+              <td className="px-6 py-4 whitespace-wrap border-r border-b border-gray-400 text-gray-400">
+                I am here to be a prompt engineer. I am a failed mixed martial
+                artist and serial murderer
+              </td>
+              <td className="px-6 py-4 whitespace-wrap border-b border-gray-400 text-gray-400">
+                Freelance
+              </td>
+            </tr>
+            {/* Add more rows as needed */}
           </tbody>
         </table>
       </div>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,15 +6,20 @@ const Navbar = () => {
     <nav>
       <div className="p-4 flex items-center justify-between">
         <Link href={"/"}>
-          <div>
-            <h1 className="font-bold text-2xl">
-              Fintech<span className="font-light">Tracker</span>
+          <div className="flex items-center justify-start">
+            <img
+              src='/logo.png'
+              height={28}
+              width={28}
+            />
+            <h1 className="font-bold text-2xl bg-gradient-to-r from-orange-400 to-orange-600 bg-clip-text text-transparent">
+              VENTURELY
             </h1>
           </div>
         </Link>
         <div className="p-4 flex items-center">
           <Link href={"/dashboard"}>
-            <div className="font-light text-xl mr-8">Dashboard</div>
+            <div className="font-light bg-gradient-to-r from-orange-400 to-orange-600 bg-clip-text text-transparent text-lg font-semibold">DASHBOARD</div>
           </Link>
         </div>
       </div>
